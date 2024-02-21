78a8039f008f11a5f6eb8781b270d258ea0ade21
Hwrn <hwrn.aou@sjtu.edu.cn>
2024-02-21 16:12:45 +0800

    style: formatting

--- workflow/MAGs/ko_relationship_N.r
+++ workflow/MAGs/ko_relationship_N.r
@@ -1,8 +1,8 @@
 ###
 #' @Date: 2022-05-18  17:46:00
 #' @Editor: Wang Jing
-#' @LastEditors: Hwrn
-#' @LastEditTime: 2022-09-01 23:47:15
+#' @LastEditors: Hwrn hwrn.aou@sjtu.edu.cn
+#' @LastEditTime: 2024-02-19 16:10:09
 #' @FilePath: /2021_09-MT10kSW/workflow/MAGs/ko_relationship_N.r
 #' @Description:
 ###
@@ -15,28 +15,34 @@ suppressMessages(library(igraph))
 #### Preprocessing                                                          ####
 ### ######################################################################## ###
 ##### INPUT: file_path, keyword_args, fig_out_path                         #####
-Wtdb_abd = argv[1]
-#Wtdb_abd = stringr::str_glue("Stdb.relative_abundance.tsv") %>% file_path$file_path$results() %>% as.character
-fig_out = argv[2]
-domain.spec = argv[3]
-#domain.spec = "Archaea"  # domain.spec = "Bacteria"  # domain.spec = "All"
+Wtdb_abd <- argv[1]
+# Wtdb_abd = stringr::str_glue("Stdb.relative_abundance.tsv") %>% file_path$file_path$results() %>% as.character
+fig_out <- argv[2]
+domain.spec <- argv[3]
+# domain.spec = "Archaea"  # domain.spec = "Bacteria"  # domain.spec = "All"
 
 ##### LOAD data AND transform TO basic format                              #####
-Stdb = load__Stdb()
-Wtdb = load__Wtdb()
-genome_taxonomy = load__genome_taxonomy(Stdb, Wtdb)
-genome.relative_abundance = get_relative_abundance(Wtdb_abd, genome_taxonomy)
+Stdb <- load__Stdb()
+Wtdb <- load__Wtdb()
+genome_taxonomy <- load__genome_taxonomy(Stdb, Wtdb)
+genome_rltabd <- get_relative_abundance(Wtdb_abd, genome_taxonomy)
 
-key_genes = load__key_genes()
-key_genes_N =
-  key_genes %>% .[.$Pathway == "N",] %>%
-  {.$Pathway = .$Arrow %>% factor(levels = unique(.)); .}
+key_genes <- load__key_genes()
+key_genes_N <- # nolint: object_name_linter.
+  key_genes %>%
+  .[.$Pathway == "N", ] %>%
+  {
+    .$Pathway <- .$Arrow %>% factor(levels = unique(.))
+    .
+  }
 
-pathway_col = set__pathway_col(key_genes_N$Pathway)
+pathway_col <- set__pathway_col(key_genes_N$Pathway)
 
-genomeko = load__genomeko()
-genomeko.key =
-  genomeko[key_genes_N$KO %>% as.character, ] %>%
+genomeko <- load__genomeko()
+genomeko_key <-
+  genomeko[
+    key_genes_N$KO %>% as.character(),
+  ] %>%
   .[apply(., 1, sum) > 0, apply(., 2, sum) > 0]
 
 
@@ -45,30 +51,42 @@ genomeko.key =
 ### ######################################################################## ###
 ##### Define function                                                      #####
 genome_jaccard_network <- function(
-  genomeko,
-  ko.spec = NA, genome.spec = NA,
-  threshold = 0.3, key_genes = key_genes
-) {
-  if (any(is.na(ko.spec))) ko.spec = rownames(genomeko)
-  else ko.spec = ko.spec %>% .[. %in% rownames(genomeko)]
-  if (any(is.na(genome.spec))) genome.spec = colnames(genomeko)
-  else genome.spec = genome.spec %>% .[. %in% colnames(genomeko)]
+    genomeko,
+    ko_spec = NA, genome_spec = NA,
+    threshold = 0.3, key_genes = key_genes) {
+  ko_spec <- if (any(is.na(ko_spec))) {
+    rownames(genomeko)
+  } else {
+    ko_spec %>% .[. %in% rownames(genomeko)]
+  }
+  genome_spec <- if (any(is.na(genome_spec))) {
+    colnames(genomeko)
+  } else {
+    genome_spec %>% .[. %in% colnames(genomeko)]
+  }
 
-  genomeko.key =
+  genomeko_key <-
     genomeko %>%
-    .[ko.spec, genome.spec] %>%
+    .[ko_spec, genome_spec] %>%
     .[apply(., 1, sum) > 0, apply(., 2, sum) > 0]
 
-  key_genes.spec = key_genes %>%
-    {rownames(.) = .$KO; .} %>% .[rownames(genomeko.key), ]
+  key_genes_spec <- key_genes %>%
+    {
+      rownames(.) <- .$KO
+      .
+    } %>%
+    .[rownames(genomeko_key), ]
 
-  m.d = vegdist(genomeko.key, method = "jaccard", binary = TRUE)
+  m_d <- vegen::vegdist(genomeko_key, method = "jaccard", binary = TRUE)
 
-  g =
+  g <-
     igraph::graph_from_adjacency_matrix(
-      1 - as.matrix(m.d), weighted = T, mode = "undirected", diag = FALSE
+      1 - as.matrix(m_d),
+      weighted = TRUE, mode = "undirected", diag = FALSE
     ) %>%
-    delete.edges(E(.) %>% .[.$weight < threshold]) %>%
+    delete.edges(
+      E(.) %>% .[.$weight < threshold]
+    ) %>%
     delete.vertices(V(.)[degree(.) == 0])
 
   plot.igraph(
@@ -76,46 +94,66 @@ genome_jaccard_network <- function(
     layout = layout_with_fr,
     edge.width = E(g)$weight,
     vertex.size = 18,
-    vertex.label = key_genes.spec$Label[match(V(g)$name, key_genes.spec$KO)],
+    vertex.label = key_genes_spec$Label[match(V(g)$name, key_genes_spec$KO)],
     vertex.label.cex = 0.8,
     vertex.label.font = 2,
     vertex.label.family = "Arial",
     vertex.color =
-      pathway_col[key_genes.spec$Pathway[match(V(g)$name, key_genes.spec$KO)]],
+      pathway_col[key_genes_spec$Pathway[match(V(g)$name, key_genes_spec$KO)]],
     vertex.frame.color = "gray",
     asp = 0
   )
 }
 
 ##### Calculate data                                                       #####
-Stdb.domain.spec =
-  genome.relative_abundance %>%
+stdb_domain_spec <-
+  genome_rltabd %>%
   {
-    if (domain.spec == "All") .
-    else .[.$Taxonomy %>% taxon.split(1, 1) %>% {. == domain.spec}, ]
+    if (domain.spec == "All") {
+      .
+    } else {
+      .[.$Taxonomy %>%
+        taxon.split(1, 1) %>%
+        {
+          . == domain.spec
+        }, ]
+    }
   } %>%
   split(.$Cluster) %>%
-  lapply(. %>% {.["Genome"] = unique(.[.$Rep == TRUE, "Genome"]); .}) %>%
+  lapply(. %>%
+    {
+      .["Genome"] <- unique(.[.$Rep == TRUE, "Genome"])
+      .
+    }) %>%
   bind_rows() %>%
-  get_taxon_group %>%
-  {.[.[unique(.$Group)] %>% apply(1, . %>% paste(collapse = "")) %>% order, ]}
+  get_taxon_group() %>%
+  {
+    .[.[unique(.$Group)] %>%
+      apply(
+        1, . %>% paste(collapse = "")
+      ) %>%
+      order(), ]
+  }
 
 ### ######################################################################## ###
 #### Plot figures and OUTPUT                                                ####
 ### ######################################################################## ###
 svg(filename = fig_out, width = 10, height = 7)
-par(mfrow = c(2, 2)) %>% {
-  for (i in names(sample_meta_col)) {
-    genome_jaccard_network(
-      genomeko.key,
-      genome.spec =
-        Stdb.domain.spec %>%
-        .[.[i] %>% apply(1, . %>% {any(. != "")}), ] %>%
-        .[, "Genome"],
-      key_genes = key_genes_N
-    )
-    title(i)
+par(mfrow = c(2, 2)) %>%
+  {
+    for (i in names(sample_meta_col)) {
+      genome_jaccard_network(
+        genomeko_key,
+        genome_spec = stdb_domain_spec %>%
+          .[.[i] %>% apply(1, . %>%
+            {
+              any(. != "")
+            }), ] %>%
+          .[, "Genome"],
+        key_genes = key_genes_N
+      )
+      title(i)
+    }
+    par(.)
   }
-  par(.)
-}
 dev.off()
--- workflow/MAGs_tpm/relative_abundance_taxon.r
+++ workflow/MAGs_tpm/relative_abundance_taxon.r
@@ -1,7 +1,7 @@
 ###
 #' @Date: 2022-06-25 10:52:06
-#' @LastEditors: Hwrn
-#' @LastEditTime: 2022-08-05 22:52:54
+#' @LastEditors: Hwrn hwrn.aou@sjtu.edu.cn
+#' @LastEditTime: 2024-02-19 14:27:50
 #' @FilePath: /2021_09-MT10kSW/workflow/MAGs_tpm/relative_abundance_taxon.r
 #' @Description:
 ###
@@ -12,21 +12,21 @@ source("workflow/utils/RLib.local/R/init.r", chdir = TRUE)
 #### Preprocessing                                                          ####
 ### ######################################################################## ###
 ##### INPUT: file_path, fig_out_path, keyword_args                         #####
-Wtdb_abd = argv[1]
-#Wtdb_abd = stringr::str_glue("Wtdb.relative_abundance.tsv") %>% file_path$file_path$results() %>% as.character
-fig_out = argv[2]
+Wtdb_abd <- argv[1]
+# Wtdb_abd = stringr::str_glue("Wtdb.relative_abundance.tsv") %>% file_path$file_path$results() %>% as.character
+fig_out <- argv[2]
 
 
 ##### GLOBAL CONST vars                                                    #####
-font_size_1 = 15
-font_size_2 = 13
-font_size_3 = 10
-axis.ticks.length = 0.1
+font_size_1 <- 15
+font_size_2 <- 13
+font_size_3 <- 10
+axis_ticks_length <- 0.1
 
 
 ##### LOAD data AND transform TO basic format                              #####
-genome_taxonomy = load__genome_taxonomy(load__Stdb(), load__Wtdb())
-genome.relative_abundance = get_relative_abundance(Wtdb_abd, genome_taxonomy)
+genome_taxonomy <- load__genome_taxonomy(load__Stdb(), load__Wtdb())
+genome_rltabd <- get_relative_abundance(Wtdb_abd, genome_taxonomy)
 
 sample_meta_cross <-
   read.csv("results/reads_diversity/metadata.tsv", sep = "\t") %>%
@@ -60,7 +60,7 @@ otu_count <-
 ##### Define function                                                      #####
 
 ##### Calculate data                                                       #####
-genome.relative_abundance %>%
+genome_rltabd %>%
   mutate(FakeCount = ceiling(get("Relative.abundance") * 1000)) %>%
   pivot_wider(
     id_cols = "Taxonomy", names_from = "Sample",
@@ -86,35 +86,38 @@ otu_count %>%
   as.data.frame() %>%
   rownames_to_column("Sample") %>%
   dplyr::arrange(get("Sample")) %>%
-  mutate(Group = gsub("^([^_]+)_.+$", "\\1", get("Sample"))) %>%
-  filter(grepl("s$", get("Group"))) %>%
-  wilcox.test(formula("S.chao1 ~ Group"), data = ., paired = FALSE)
+  mutate(
+    Area = ifelse(grepl("^(S[^_])_.+$", get("Sample")), "slope", "bottom")
+  ) %>%
+  wilcox.test(formula("S.chao1 ~ Area"), data = ., paired = FALSE)
 
 ### ######################################################################## ###
 #### Plot figures and OUTPUT                                                ####
 ### ######################################################################## ###
 ##### Plot figures                                                         #####
-p1 = get_total_plot(genome.relative_abundance, "Relative_abundance",
-                    labs.x = "total mapping rate")
+p1 <- get_total_plot(genome_rltabd, "Relative_abundance",
+  labs.x = "total mapping rate"
+)
 
-taxon_color = load__taxon_color()
+taxon_color <- load__taxon_color()
 
-p2 =
-  genome.relative_abundance %>%
-  {.$Taxonomy = sapply(taxon.split(.$Taxonomy, 1, 7), get_taxon_color); .} %>%
+p2 <-
+  genome_rltabd %>%
+  mutate(
+    Taxonomy = sapply(taxon.split(get("Taxonomy"), 1, 7), get_taxon_color)
+  ) %>%
   get_percent_plot("Relative_abundance", "Taxonomy",
-                   labs.x = "Layer", labs.y = "relative abundance") +
+    labs.x = "Layer", labs.y = "relative abundance"
+  ) +
   scale_fill_manual(
-    values =
-      c(taxon_color$LEGEND_COLORS) %>%
-      {names(.) = taxon_color$LEGEND_LABELS; .} %>%
+    values = c(taxon_color$LEGEND_COLORS) %>%
+      `names<-`(taxon_color$LEGEND_LABELS) %>%
       .[order(names(.))]
   )
 
 
 ##### OUTPUT                                                               #####
-pout =
-  p1 + p2 + guides(fill = "none") +
+pout <- p1 + p2 + guides(fill = "none") +
   plot_layout(heights = c(1, 4), nrow = 2, guides = "collect") +
   plot_annotation(tag_levels = "A", tag_prefix = "(", tag_suffix = ")") &
   theme(plot.tag = element_text(size = 18))
